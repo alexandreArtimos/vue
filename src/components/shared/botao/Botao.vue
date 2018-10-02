e8cc91eacb856cca72dc1ae86835c99709329476
alexandre <alexandre.artimos@gmail.com>
2018-10-02 07:42:06 -0300

    Subindo codigo

---
+++ src/components/shared/botao/Botao.vue
@@ -0,0 +1,42 @@
+<template>
+    <button @click="disparaAcao()" class="botao" :class="estiloDoBotao" :type="tipo">{{rotulo}}</button>
+    
+</template>
+<script>
+export default {
+  props: {
+    tipo: {
+      required: true,
+      type: String
+    },
+    rotulo: {
+      required: true,
+      type: String
+    },
+    confirmacao: Boolean,
+    estilo: String
+  },
+  methods: {
+    disparaAcao() {
+      if (this.confirmacao) {
+        if (confirm("Confirma operecao ?")) {
+          this.$emit("botaoAtivado", new Date());
+        }
+        return;
+      }
+      this.$emit("botaoAtivado", new Date());
+    }
+  },
+  computed: {
+    estiloDoBotao() {
+      if (this.estilo == "padrao" || !this.estilo) return "botao-padrao";
+      if (this.estilo == "perigo") return "botao-perigo";
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+  @import './Botao.scss';
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import {routes} from './routes';
+import './directives/Transform';
+
+// importando o VeeValidate 
+import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/teste.css';
+
+import './assets/js/teste.js';
+
+import 'jquery/dist/jquery'
+import 'bootstrap/dist/js/bootstrap.js';
+
+Vue.use(VueResource);
+Vue.http.options.root = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000';
+
+Vue.http.interceptors.push((req, next) => {
+
+  // é possível colocar informações no header antes do envio da requisição
+  //req.headers.set('Authorization', 'informação de segurança aqui');
+  console.log('Lidando com o request');
+
+  next(res => {
+    console.log('Lidando com a resposta')
+    // é possível acessar os dados da reposta e realizar transformações antes
+    console.log(res.body);
+  });
+
+});
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  //routes:routes
+  //ECMA 6
+  routes,
+  mode: 'history'
+})
+
+VeeValidate.Validator.localize('pt_BR',msg);
+Vue.use(VeeValidate);
+
+
+new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(App)
+})
